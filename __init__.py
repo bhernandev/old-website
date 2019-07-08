124284d32f3e018d467c162370e0022b65722d1d
bhernandev <brian@hernandez-cs.com>
2019-07-07 23:29:53 -0400

    add bean project as submodule to website

--- __init__.py
+++ __init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask import render_template
+
+from .bean.views import bean
+
 app = Flask(__name__, static_url_path='/static')
+app.register_blueprint(bean, url_prefix='/games/bean')
 
 @app.route('/')
 def index():
-	return render_template('index.html');
+	return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
